8bd89c763cb46a0ce3782047365d07204c256d68
jam-madrigal <jam.madrigal@gmail.com>
2022-02-10 00:14:53 -0800

    Add async function to read kepler stream data and push it to an array

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Planets
 A project using data from the Kepler space telescope and nodejs.
+
+- Using the streaming capabilities of node to read the Kepler data
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const { parse } = require('csv-parse'); 
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler-data.csv')
+    .on('data', (data) => {
+        results.push(data);
+    });
+// parse();
