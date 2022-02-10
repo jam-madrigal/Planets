a6c984ddab75816223c6eadb269cbff51a525644
jam-madrigal <jam.madrigal@gmail.com>
2022-02-10 00:17:39 -0800

    Add handler for end event to log read data stream array

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const results = [];
 fs.createReadStream('kepler-data.csv')
     .on('data', (data) => {
         results.push(data);
-    });
+    })
+    .on('end', (results) => {
+        console.log(results);
+        console.log("Done reading data stream")
+    })
+
 // parse();
