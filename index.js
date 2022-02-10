cf4b28cfbc9457d30755835a5ab1e6f2d0f88c30
jam-madrigal <jam.madrigal@gmail.com>
2022-02-10 00:20:40 -0800

    Add comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { parse } = require('csv-parse'); 
 const fs = require('fs');
 
+// Array to store the results of our kepler data read with node's api
 const results = [];
 
+// Using node to read our kepler data csv file, adding handlers to push the results to an array and log them, as well as catch any errors
 fs.createReadStream('kepler-data.csv')
     .on('data', (data) => {
         results.push(data);
